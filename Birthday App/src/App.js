d5b55d413581f36baf63f9349f95ae74e939a041
knelavel13 <89713981+knelavel@users.noreply.github.com>
2023-08-24 00:08:09 -0500

    Update App.js

--- Birthday App/src/App.js
+++ Birthday App/src/App.js
@@ -1,1 +1,26 @@
+import React, {useState} from 'react'
+import data from './data'
+import './App.css';
+import List from './List.js'
+
+
+const App = () => {
+const[people, setPeople] = useState(data)
+
+
+const handleClick= (e) =>{
+   e.preventDefault()
+   setPeople([])
+}
+  return (
+    <div className="Container">
+      <h2>{people.length} Birthdays today</h2>
+      <List people={people} />
+      <button className="btn" onClick={handleClick}>Clear All</button>
+    
+    </div>
+  )
+}
+
+export default App
 
